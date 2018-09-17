22bcbb1a998a85fb81a6bc45ee8fc0e6ac3a30c2
João Felipe Gobeti Calenzani <joao.calen@hotmail.com>
2018-09-17 19:56:40 -0300

    Iniciando projeto

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=netmap
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/netmap
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=netmap.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/netmap.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=netmap
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/netmap
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=netmap.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/netmap.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
